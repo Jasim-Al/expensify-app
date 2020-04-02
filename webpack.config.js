f15bea1d39680a22d1ffa6ce5cbc3040254b3e20
Jasim-Al <59736291+Jasim-Al@users.noreply.github.com>
2020-04-02 21:46:14 +0530

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ if ( process.env.NODE_ENV === 'test' ) {
     require('dotenv').config({ path : '.env.test' });
 }else if ( process.env.NODE_ENV === 'development' ) {
     require('dotenv').config({ path : '.env.development' });
-}
+};
 
 module.exports = (env) => {
 
@@ -19,6 +19,16 @@ module.exports = (env) => {
             new MiniCssExtractPlugin({
                 filename: 'styles.css',
               }),
+              new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                'process.env.FIREBASE_APP_ID' : JSON.stringify(process.env.FIREBASE_APP_ID)
+
+            })
         ],
         mode:'development',
         entry:'./src/app.js',
@@ -55,18 +65,6 @@ module.exports = (env) => {
                 ]
             }]
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-                'process.env.FIREBASE_APP_ID' : JSON.stringify(process.env.FIREBASE_APP_ID)
-
-            })
-        ],
         devtool: isProduction ? 'source-map' : "inline-source-map",
         devServer:{
             contentBase:path.join(__dirname, 'public'),
